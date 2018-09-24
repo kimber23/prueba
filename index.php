c76f473c74c3c49a68996d8a1231544f9913d068
kimber <kycerq.2015@gmail.com>
2018-09-24 17:26:07 -0500

    se agrego una tabla a la pagina index.php

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 	<title>hola mundo</title>
 </head>
 <body>
- hello world
+ hello world}
+ <table>
+ 	<tr><td>hola</td><td>como estas</td></tr>
+ 	<tr><td>muy bien</td><td>thank you!</td></tr>
+
+ </table>
 </body>
 </html>
